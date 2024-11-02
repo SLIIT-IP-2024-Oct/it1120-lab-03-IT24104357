c9122b66c2d0b8dcab0692b3afc7a2f96b804740
IT24104357 <it24104357@my.sliit.lk>
2024-11-02 13:00:06 +0530

    Add files via upload

---
+++ Q1A/IT241043573Q1A.java
@@ -0,0 +1,11 @@
+public class IT241043573Q1A
+{
+	public static void main(String[] args)
+	{
+		double price_of_1kg_of_rice,number_of_kilograms,amount_you_have_to_pay;
+		price_of_1kg_of_rice=250;
+		number_of_kilograms=4;
+		amount_you_have_to_pay=price_of_1kg_of_rice*number_of_kilograms;
+		System.out.println(amount_you_have_to_pay);
+	}
+}
